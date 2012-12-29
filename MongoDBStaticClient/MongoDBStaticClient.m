707665ffa684b737bbb56632125d177731545e91
Daniel Parnell <me@danielparnell.com>
2012-12-29 16:31:24 +1100

    Added static library target

---
+++ MongoDBStaticClient/MongoDBStaticClient.m
@@ -0,0 +1,13 @@
+//
+//  MongoDBStaticClient.m
+//  MongoDBStaticClient
+//
+//  Created by Daniel Parnell on 29/12/12.
+//  Copyright (c) 2012 Automagic Software Pty Ltd. All rights reserved.
+//
+
+#import "MongoDBStaticClient.h"
+
+@implementation MongoDBStaticClient
+
+@end
